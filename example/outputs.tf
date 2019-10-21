a5b5449a56cc21cc405fa37419707c4ab95e4d69
Yomi Ogunyinka <kbab6aoo@hotmail.com>
2019-10-21 18:26:17 +0100

    outputs added

---
+++ example/outputs.tf
@@ -0,0 +1,33 @@
+output "vpc_name" {
+  value = module.vpc.vpc_name
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "region" {
+  value = "${var.region}"
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of public subnets"
+  value       = module.vpc.public_subnets_cidr_blocks
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = module.vpc.private_subnets_cidr_blocks
+}
