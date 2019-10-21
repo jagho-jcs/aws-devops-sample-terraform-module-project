b6ff74e5b3c8801e30467b9c724774f9ebfb880f
kbab6aoo <kbab6aoo@hotmail.com>
2019-10-21 16:32:39 +0100

    changed hard coded values to variables

---
+++ example/main.tf
@@ -0,0 +1,20 @@
+data "aws_availability_zones" "all" {}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "vpc" {
+  source = "../"
+
+  name                       = var.name
+  
+  cidr                       = var.cidr
+
+  azs                        = var.azs
+  
+  public_subnets             = var.public_subnets
+
+  private_subnets            = var.private_subnets
+}
