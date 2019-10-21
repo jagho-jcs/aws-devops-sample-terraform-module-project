1d014d4ea3d96063d1cc86ece1a78411b0c80512
Yomi Ogunyinka <kbab6aoo@hotmail.com>
2019-10-21 18:26:44 +0100

    outputs added

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,31 @@
 output "vpc_id" {
-  value = "${aws_vpc.this[0].id}"
+  value = aws_vpc.this[0].id
+}
+
+output "vpc_name" {
+  value = var.name
+}
+
+output "vpc_cidr_block" {
+  value = aws_vpc.this.*.cidr_block
+}
+
+output "public_subnets" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.public.*.id
+}
+
+output "public_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = aws_subnet.public.*.cidr_block
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private.*.id
+}
+
+output "private_subnets_cidr_blocks" {
+  description = "List of cidr_blocks of private subnets"
+  value       = aws_subnet.private.*.cidr_block
 }
