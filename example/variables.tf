f6662484c411522995bc5212c6d8008de8ef4cef
Yomi Ogunyinka <kbab6aoo@hotmail.com>
2019-10-22 12:25:46 +0100

    unused tags removed

--- example/variables.tf
+++ example/variables.tf
@@ -1,8 +1,26 @@
+#######################################
+#
+# Environment Variables
+#
+#######################################
+
 variable "region" {}
 variable "shared_credentials_file" {}
 variable "profile" {}
+
+#######################################
+
 variable "name" {}
 variable "cidr" {}
 variable "azs" {}
 variable "private_subnets" {}
 variable "public_subnets" {}
+
+#######################################
+#
+# Tags
+#
+#######################################
+
+variable "environment_tag" {}
+variable "vpc_tags" {}
